eece2bf1a2a2a123799d1ee24f3f2fe5d93e06a5
geamedical <geamedical11@gmail.com>
2024-01-03 14:02:26 +0700

    3 januari 2024 14:02

--- app/Services/Ws.ts
+++ app/Services/Ws.ts
@@ -1,11 +1,11 @@
 import { Server } from 'socket.io'
 import AdonisServer from '@ioc:Adonis/Core/Server'
-import Env from '@ioc:Adonis/Core/Env'
+// import Env from '@ioc:Adonis/Core/Env'
 
 class Ws {
     public io: Server
     private booted = false
-    private frontend:string;
+    // private frontend: string;
 
     public boot() {
         if (this.booted) {
@@ -16,16 +16,18 @@ class Ws {
         // } else {
         //     this.frontend = `${Env.get('PROTOCOL_FRONTEND')}://${Env.get('HOST_FRONTEND')}:${Env.get('PORT_FRONTEND')}`
         // }
-        this.frontend = `${Env.get('PROTOCOL_FRONTEND')}://${Env.get('HOST_FRONTEND')}:${Env.get('PORT_FRONTEND')}`
-        this.booted = true
-        this.io = new Server(AdonisServer.instance, {
-            cors: {
-                origin: this.frontend,
-                credentials: true,
-            },
-            allowEIO3: true
-        })
+        // this.frontend = `${Env.get('PROTOCOL_FRONTEND')}://${Env.get('HOST_FRONTEND')}:${Env.get('PORT_FRONTEND')}`
+        // this.booted = true
+        // this.io = new Server(AdonisServer.instance, {
+        //     cors: {
+        //         origin: this.frontend,
+        //         credentials: true,
+        //     },
+        //     allowEIO3: true
+        // })
         // this.io = new Server(AdonisServer.instance!)
+        this.booted = true
+        this.io = new Server(AdonisServer.instance!)
     }
 }
 export default new Ws()
